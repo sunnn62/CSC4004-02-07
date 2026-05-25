3d6f2bad5ae0de9c89d7b2f63128c1e73a3bc49f
김은조 <uncho211@gmail.com>
2026-05-25 21:57:10 +0900

    chore: sync D code (MidiComparatorService) from feat/sh

---
+++ frontend/js/d/midi/chordBuffer.js
@@ -0,0 +1,32 @@
+/**
+ * 50ms 윈도우 내 MIDI 입력을 화음으로 묶어서 전달
+ */
+
+export class ChordBuffer {
+  constructor(windowMs = 50) {
+    this.windowMs = windowMs;
+    this._buffer = [];    // { note, velocity }[]
+    this._timer = null;
+    this._timestamp = null;
+    this.onChord = null;  // (notes: number[], velocity: number, timestamp: number) => void
+  }
+
+  push(midiNote, velocity, timestamp) {
+    if (this._buffer.length === 0) {
+      this._timestamp = timestamp; // 화음의 타임스탬프는 첫 음 기준
+    }
+    this._buffer.push({ note: midiNote, velocity });
+
+    clearTimeout(this._timer);
+    this._timer = setTimeout(() => this._flush(), this.windowMs);
+  }
+
+  _flush() {
+    if (this._buffer.length === 0) return;
+    const notes = this._buffer.map((b) => b.note);
+    const velocity = Math.max(...this._buffer.map((b) => b.velocity));
+    this.onChord?.(notes, velocity, this._timestamp);
+    this._buffer = [];
+    this._timestamp = null;
+  }
+}
